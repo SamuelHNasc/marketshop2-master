21981f5e6479c9e77f408cc44940a1d1ed10782e
SamuelHNasc <samuelhn.contato@gmail.com>
2024-06-13 10:24:51 -0300

    Perfil

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -10,7 +10,7 @@
       <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
-            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img alt="Tailwind CSS Navbar component" src="https://cdn-icons-png.flaticon.com/256/4825/4825044.png" />
           </div>
         </div>
         <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
